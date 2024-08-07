a5ab9868f029978d486c8b2af7ddd6510f3c0940
amrit-44404 <amrit44404@proton.me>
2024-08-07 14:42:00 +0545

    dependencies to install dwm

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ cat << "EOF"
 EOF
 
 # Install essential packages
-sudo pacman -S brightnessctl xwallpaper htop lf xorg-xset alsa-utils ttf-font-awesome ttf-hack ttf-hack-nerd picom fastfetch firefox nsxiv neovim mpv newsboat sxhkd bleachbit unzip zathura zathura-pdf-poppler
+sudo pacman -S brightnessctl xwallpaper htop lf xorg-xset alsa-utils ttf-font-awesome ttf-hack ttf-hack-nerd picom fastfetch firefox nsxiv neovim mpv newsboat sxhkd bleachbit unzip zathura zathura-pdf-poppler libxft libxinerama
 
 # Clone dotfiles repository
 git clone --depth=1 https://gitlab.com/amrit-44404/archrice $HOME/archrice
