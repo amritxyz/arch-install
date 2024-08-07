e1fb9b0717fa47fe92c7f59d6737f935fc5b85e6
amrit-44404 <amrit44404@proton.me>
2024-08-07 23:23:17 +0545

    adding gtk config

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ EOF
 # Install essential packages
 sudo pacman -S brightnessctl xwallpaper htop lf xorg-xset xdotool alsa-utils ttf-font-awesome ttf-hack \
 	ttf-hack-nerd noto-fonts-emoji picom fastfetch firefox nsxiv neovim mpv newsboat sxhkd bleachbit unzip \
-	zathura zathura-pdf-poppler libxft libxinerama scrot
+	zathura zathura-pdf-poppler libxft libxinerama scrot arc-gtk-theme arc-icon-theme
 
 # Clone dotfiles repository
 git clone --depth=1 https://gitlab.com/amrit-44404/archrice $HOME/archrice
